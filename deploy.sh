19f8c679b5f580a5c95f05092cdf0a409e5f498e
pli01 <pli01@github.com>
2018-03-27 16:39:23 +0000

    use hostname

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ export SERVICE_CI_TOOL_STACK_ENABLE=${SERVICE_CI_TOOL_STACK_ENABLE:-true}
 
 echo "Configuration started"
 time ( cd $(dirname $0)/ansible &&
-  ansible-playbook -i config -c local -l localhost \
+  ansible-playbook -i config -c local -l $(hostname -s) \
      -e action=install \
      playbooks/site.yml -v
 )
