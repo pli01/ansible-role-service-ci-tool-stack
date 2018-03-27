df477705dc2734a77f8a8c944271a8f1e2b18730
pli01 <pli01@github.com>
2018-03-27 13:43:48 +0000

    optionnal galaxy args

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 cd $(dirname $0) || exit 1
+
+ANSIBLE_GALAXY_ARGS=${ANSIBLE_GALAXY_FORCE:+" -f "}
+
 # git needed if requirements contains git repo
 if [ -f ansible/requirements.yml ] ; then
-( cd ansible && ansible-galaxy install -r requirements.yml -p roles -f -vvv )
+( cd ansible && ansible-galaxy install -r requirements.yml -p roles $ANSIBLE_GALAXY_ARGS -vvv )
 fi
 
