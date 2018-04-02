c1db7b016d27829d1f1212f4857579156d6a836a
pli <filib@laposte.net>
2018-04-02 19:40:53 +0200

    Add nexus/jenkins service in multinode mode

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,15 @@ set -e
 
 # Activate configuration playbook of following services
 export SERVICE_CI_TOOL_STACK_ENABLE=${SERVICE_CI_TOOL_STACK_ENABLE:-true}
+export ANSIBLE_DEPLOY_LOCAL=${ANSIBLE_DEPLOY_LOCAL:-$(hostname -s)}
 
 echo "Configuration started"
 time ( cd $(dirname $0)/ansible &&
-  ansible-playbook -i config -c local -l $(hostname -s) \
+  ansible-playbook -i config -c local -l ${ANSIBLE_DEPLOY_LOCAL} \
+     -e action=install \
+     playbooks/site.yml --list-tasks --list-hosts  && \
+  ansible-playbook -i config -c local -l ${ANSIBLE_DEPLOY_LOCAL} \
      -e action=install \
      playbooks/site.yml -v
+
 )
