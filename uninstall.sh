469215e746ac0466ddff82935cdfcd5a3aafbd38
pli01 <philippe.libat@free.fr>
2018-04-08 18:50:22 +0200

    uninstall: disable unit systemd

--- uninstall.sh
+++ uninstall.sh
@@ -3,10 +3,21 @@ set -e
 
 # Activate configuration playbook of following services
 export SERVICE_CI_TOOL_STACK_ENABLE=${SERVICE_CI_TOOL_STACK_ENABLE:-true}
+export SERVICE_CI_NEXUS_STACK_ENABLE=${SERVICE_CI_NEXUS_STACK_ENABLE:-true}
+export SERVICE_CI_JENKINS_STACK_ENABLE=${SERVICE_CI_JENKINS_STACK_ENABLE:-true}
+export SERVICE_CI_GITLAB_STACK_ENABLE=${SERVICE_CI_GITLAB_STACK_ENABLE:-true}
+export SERVICE_CI_FRONT_STACK_ENABLE=${SERVICE_CI_FRONT_STACK_ENABLE:-true}
+export SERVICE_CI_ELK_STACK_ENABLE=${SERVICE_CI_ELK_STACK_ENABLE:-true}
+
+export ANSIBLE_DEPLOY_LOCAL=${ANSIBLE_DEPLOY_LOCAL:-$(hostname -s)}
 
 echo "Configuration started"
 time ( cd $(dirname $0)/ansible &&
-  ansible-playbook -i config -c local -l localhost \
+  ansible-playbook -i config -c local -l ${ANSIBLE_DEPLOY_LOCAL} \
      -e action=uninstall \
+     playbooks/uninstall.yml --list-tasks --list-hosts  && \
+  ansible-playbook -i config -c local -l ${ANSIBLE_DEPLOY_LOCAL} \
+     -e action=install \
      playbooks/uninstall.yml -v
+
 )
