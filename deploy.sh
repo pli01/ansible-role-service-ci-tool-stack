1318be545457a954d8d500c2073ce9ad1c1bfe4c
pli01 <pli01@github.com>
2018-06-28 12:32:13 +0000

    disable host key checking , rename ANSIBLE_DEPLOY_LOCAL to ANSIBLE_DEPLOY_LIMIT

--- deploy.sh
+++ deploy.sh
@@ -8,15 +8,17 @@ export SERVICE_CI_JENKINS_STACK_ENABLE=${SERVICE_CI_JENKINS_STACK_ENABLE:-true}
 export SERVICE_CI_GITLAB_STACK_ENABLE=${SERVICE_CI_GITLAB_STACK_ENABLE:-true}
 export SERVICE_CI_FRONT_STACK_ENABLE=${SERVICE_CI_FRONT_STACK_ENABLE:-true}
 export SERVICE_CI_ELK_STACK_ENABLE=${SERVICE_CI_ELK_STACK_ENABLE:-true}
-
-export ANSIBLE_DEPLOY_LOCAL=${ANSIBLE_DEPLOY_LOCAL:-$(hostname -s)}
+export ANSIBLE_DEPLOY_LIMIT=${ANSIBLE_DEPLOY_LIMIT:-$(hostname -s)}
+# use connection smart for ssh or local
+export ANSIBLE_CONNECTION=${ANSIBLE_CONNECTION:-local}
+export ANSIBLE_HOST_KEY_CHECKING=False
 
 echo "Configuration started"
 time ( cd $(dirname $0)/ansible &&
-  ansible-playbook -i config -c local -l ${ANSIBLE_DEPLOY_LOCAL} \
+  ansible-playbook -i config -c ${ANSIBLE_CONNECTION} -l ${ANSIBLE_DEPLOY_LIMIT} \
      -e action=install \
      playbooks/site.yml --list-tasks --list-hosts  && \
-  ansible-playbook -i config -c local -l ${ANSIBLE_DEPLOY_LOCAL} \
+  ansible-playbook -i config -c ${ANSIBLE_CONNECTION} -l ${ANSIBLE_DEPLOY_LIMIT} \
      -e action=install \
      playbooks/site.yml -v
 
